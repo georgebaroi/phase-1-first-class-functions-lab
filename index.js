9a25927d494fe493eb16dbfbcbe506be8fd91964
George Baroi <baroi.george@gmail.com>
2022-07-21 19:50:16 -0400

    commit tree 0ddf8cec1cad913b41f33fdb19ec6532a1634f65

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+//create function that takes in an array and returns the first two obejcts (anonymous)
+//set const variable
+let drivers = []
+
+const returnFirstTwoDrivers = function(drivers){
+return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = function(){
+    return selectingDrivers.apply(returnFirstTwoDrivers, returnLastTwoDrivers)
+}
+
+
